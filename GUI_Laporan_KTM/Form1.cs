1217e8e129e9bde531f9c33b69cea2f3e30bec45
purianagatha <purianagatha@gmail.com>
2024-06-02 16:01:35 +0700

    Page create laporan beres

--- GUI_Laporan_KTM/Form1.cs
+++ GUI_Laporan_KTM/Form1.cs
@@ -16,5 +16,20 @@ namespace GUI_Laporan_KTM
         {
             InitializeComponent();
         }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
