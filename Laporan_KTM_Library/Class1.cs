201735a77d7f499cb691e2d935c8998097606f62
purianagatha <purianagatha@gmail.com>
2024-06-02 15:16:25 +0700

    init empty Laporan KTM Library

---
+++ Laporan_KTM_Library/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laporan_KTM_Library
+{
+    public class Class1
+    {
+    }
+}
