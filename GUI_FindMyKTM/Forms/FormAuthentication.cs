7731a024ab70445ac6bed3339d36e20044651ee6
yazidalg <yazid.al2418@gmail.com>
2024-06-13 03:33:59 +0700

    update login form

--- GUI_FindMyKTM/Forms/FormAuthentication.cs
+++ GUI_FindMyKTM/Forms/FormAuthentication.cs
@@ -16,5 +16,15 @@ namespace GUI_FindMyKTM.Forms
         {
             InitializeComponent();
         }
+
+        private void tabPage1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox6_TextChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
