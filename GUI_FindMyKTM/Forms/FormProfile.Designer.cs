ffbc2566eb112db76c8c04b518fd57b2f4c24449
Jeremy Fabian <118284208+LkyFabian@users.noreply.github.com>
2024-06-14 08:51:43 +0700

    baru

--- GUI_FindMyKTM/Forms/FormProfile.Designer.cs
+++ GUI_FindMyKTM/Forms/FormProfile.Designer.cs
@@ -31,25 +31,27 @@
             this.label1 = new System.Windows.Forms.Label();
             this.LabelNim = new System.Windows.Forms.Label();
             this.LabelNama = new System.Windows.Forms.Label();
-            this.PlaceHolder = new System.Windows.Forms.Label();
+            this.tempatNama = new System.Windows.Forms.Label();
+            this.tempatNim = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(238, 140);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Location = new System.Drawing.Point(476, 269);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.Size = new System.Drawing.Size(0, 25);
             this.label1.TabIndex = 0;
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // LabelNim
             // 
             this.LabelNim.AutoSize = true;
-            this.LabelNim.Location = new System.Drawing.Point(44, 181);
+            this.LabelNim.Location = new System.Drawing.Point(88, 348);
+            this.LabelNim.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.LabelNim.Name = "LabelNim";
-            this.LabelNim.Size = new System.Drawing.Size(23, 13);
+            this.LabelNim.Size = new System.Drawing.Size(46, 25);
             this.LabelNim.TabIndex = 1;
             this.LabelNim.Text = "nim";
             this.LabelNim.Click += new System.EventHandler(this.label2_Click);
@@ -57,33 +59,46 @@
             // LabelNama
             // 
             this.LabelNama.AutoSize = true;
-            this.LabelNama.Location = new System.Drawing.Point(44, 71);
+            this.LabelNama.Location = new System.Drawing.Point(88, 137);
+            this.LabelNama.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.LabelNama.Name = "LabelNama";
-            this.LabelNama.Size = new System.Drawing.Size(35, 13);
+            this.LabelNama.Size = new System.Drawing.Size(68, 25);
             this.LabelNama.TabIndex = 2;
             this.LabelNama.Text = "Nama";
             this.LabelNama.Click += new System.EventHandler(this.label3_Click);
             // 
-            // PlaceHolder
+            // tempatNama
             // 
-            this.PlaceHolder.AutoSize = true;
-            this.PlaceHolder.Location = new System.Drawing.Point(101, 71);
-            this.PlaceHolder.Name = "PlaceHolder";
-            this.PlaceHolder.Size = new System.Drawing.Size(35, 13);
-            this.PlaceHolder.TabIndex = 3;
-            this.PlaceHolder.Text = "Nama";
-            this.PlaceHolder.Click += new System.EventHandler(this.label2_Click_1);
+            this.tempatNama.AutoSize = true;
+            this.tempatNama.Location = new System.Drawing.Point(88, 213);
+            this.tempatNama.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.tempatNama.Name = "tempatNama";
+            this.tempatNama.Size = new System.Drawing.Size(140, 25);
+            this.tempatNama.TabIndex = 3;
+            this.tempatNama.Text = "TempatNama";
+            this.tempatNama.Click += new System.EventHandler(this.label2_Click_1);
+            // 
+            // tempatNim
+            // 
+            this.tempatNim.AutoSize = true;
+            this.tempatNim.Location = new System.Drawing.Point(93, 431);
+            this.tempatNim.Name = "tempatNim";
+            this.tempatNim.Size = new System.Drawing.Size(70, 25);
+            this.tempatNim.TabIndex = 4;
+            this.tempatNim.Text = "label3";
+            this.tempatNim.Click += new System.EventHandler(this.label2_Click_2);
             // 
             // FormProfile
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(632, 372);
-            this.Controls.Add(this.PlaceHolder);
+            this.ClientSize = new System.Drawing.Size(1264, 715);
+            this.Controls.Add(this.tempatNim);
+            this.Controls.Add(this.tempatNama);
             this.Controls.Add(this.LabelNama);
             this.Controls.Add(this.LabelNim);
             this.Controls.Add(this.label1);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "FormProfile";
             this.Text = "FormLaporan";
             this.Load += new System.EventHandler(this.FormProfile_Load);
@@ -97,6 +112,7 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label LabelNim;
         private System.Windows.Forms.Label LabelNama;
-        private System.Windows.Forms.Label PlaceHolder;
+        private System.Windows.Forms.Label tempatNama;
+        private System.Windows.Forms.Label tempatNim;
     }
 }
--- GUI_FindMyKTM/Forms/FormProfile.cs
+++ GUI_FindMyKTM/Forms/FormProfile.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,9 +13,57 @@ namespace GUI_FindMyKTM.Forms
 {
     public partial class FormProfile : Form
     {
+        private HttpClient httpClient;
+        private const string apiUrl = "http://192.168.18.13:9000/api/report";
         public FormProfile()
         {
             InitializeComponent();
+            LoadReportDetailsFromApi();
+            httpClient = new HttpClient();
+        }
+        private async void LoadReportDetailsFromApi()
+        {
+            try
+            {
+                HttpResponseMessage response = await httpClient.GetAsync(apiUrl);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    string responseBody = await response.Content.ReadAsStringAsync();
+                    var responseData = Newtonsoft.Json.JsonConvert.DeserializeObject<dynamic>(responseBody);
+
+                    int dataLength = responseData.data.Count;
+
+
+                    if (dataLength > 0)
+                    {
+
+                        var lastReport = responseData.data[dataLength - 1];
+
+                        string title = lastReport.student.name;
+
+                        string nim = lastReport.student.nim;
+                        string alasan = lastReport.student.alasan;
+
+                        // tempatFakultas = lastReport.student.faculty;
+                        tempatNama.Text = lastReport.student.name;
+                        tempatNim.Text = lastReport.student.nim;
+
+                    }
+                }
+                else
+                {
+                    tempatNim.Text = "gagal meload data"; // jika gagal meload data
+                }
+            }
+            catch (HttpRequestException ex)
+            {
+                tempatNim.Text = $"Error: {ex.Message}";
+            }
+            catch (Exception ex)
+            {
+                tempatNim.Text = $"Error: {ex.Message}";
+            }
         }
 
         private void label1_Click(object sender, EventArgs e)
@@ -38,6 +87,11 @@ namespace GUI_FindMyKTM.Forms
         }
 
         private void label2_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click_2(object sender, EventArgs e)
         {
 
         }
